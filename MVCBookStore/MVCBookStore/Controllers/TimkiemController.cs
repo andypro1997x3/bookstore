9b59d9be6f4c2c8c9f5c1359a142212c73a5f39b
Nhat <andypro1997x3@gmail.com>
2018-05-12 22:30:04 +0700

    bai1

---
+++ MVCBookStore/MVCBookStore/Controllers/TimkiemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBookStore.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace MVCBookStore.Controllers
+{
+    public class TimkiemController : Controller
+    {
+        // GET: Timkiem
+        dbQLBansachDataContext db = new dbQLBansachDataContext();
+        [HttpPost]
+        public ActionResult Ketquatimkiem(FormCollection collection, int ? page)
+        {
+            string tukhoa = collection["txttimkiem"].ToString();
+            ViewBag.Tukhoa = tukhoa;
+            List<SACH> lstkqtk = db.SACHes.Where(n => n.Tensach.Contains(tukhoa)).ToList();
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            if (lstkqtk.Count == 0)
+            {
+                ViewBag.Thongbao = "Không tìm thấy sản phẩm nào";
+                return View(db.SACHes.OrderBy(n => n.Tensach).ToPagedList(pageNum, pageSize));
+            }
+            ViewBag.Thongbao = "Đã tìm thấy" + lstkqtk.Count + "Kết quả";
+            return View(lstkqtk.OrderBy(n=>n.Tensach).ToPagedList(pageNum, pageSize));
+        }
+        [HttpGet]
+        public ActionResult Ketquatimkiem(string tukhoa, int? page)
+        {
+            ViewBag.Tukhoa = tukhoa;
+            List<SACH> lstkqtk = db.SACHes.Where(n => n.Tensach.Contains(tukhoa)).ToList();
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            if (lstkqtk.Count == 0)
+            {
+                ViewBag.Thongbao = "Không tìm thấy sản phẩm nào";
+                return View(db.SACHes.OrderBy(n => n.Tensach).ToPagedList(pageNum, pageSize));
+            }
+            ViewBag.Thongbao = "Đã tìm thấy" + lstkqtk.Count + "Kết quả";
+            return View(lstkqtk.OrderBy(n => n.Tensach).ToPagedList(pageNum, pageSize));
+        }
+    }
+}
